50c76ae3fce1798fd6ca817fa87b32911cb8f11c
Simon Birchall <sbirchall@Simons-MacBook-Pro.local>
2017-01-22 12:01:42 +0000

    Enforce user uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
-    validates :name, presence: true
-    validates :name, length: { maximum: 50 }
-    validates :email, presence: true
-    validates :email, length: { maximum: 255 }
-    validates :email, format: { with: VALID_EMAIL_REGEX }
+    validates :name,    presence: true,
+                        length: { maximum: 50 }
+    validates :email,   presence: true,
+                        length: { maximum: 255 },
+                        format: { with: VALID_EMAIL_REGEX },
+                        uniqueness: true
 end
