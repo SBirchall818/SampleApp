a3d2fb58192ba40be71350949273a20a6f5c0783
Simon Birchall <sbirchall@Simons-MacBook-Pro.local>
2017-01-22 10:37:40 +0000

    Add string length validatons to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
     validates :name, presence: true
+    validates :name, length: { maximum: 50 }
     validates :email, presence: true
+    validates :email, length: { maximum: 255 }
 end
